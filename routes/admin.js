2ed9eca85839c0d9f45d448a667789924b01abb1
Jayraj Deshmukh <jayrajdeshmukh4199@gmail.com>
2025-12-22 18:57:39 +0530

    Update Admine

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/enrolled-students/:c_id",(req,res)=>{
-   const sql= "select *  from students where course-id=?"
+   const sql= "select *  from students where course_id=?"
     const cid=req.params.c_id 
     pool.query(sql,[cid],(err,data)=>{
         if(err){
@@ -15,3 +15,5 @@ router.get("/enrolled-students/:c_id",(req,res)=>{
                  res.send(result.createResult(null,data))
     })
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const courseRoutes=require('./routes/courseRoutes')
 const studrouter=require("./routes/student")
 const videoRoutes=require("./routes/videoRouters")
+const admin=require("./routes/admin")
 
 const app=express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use('/courses',courseRoutes)
 app.use("/student",studrouter)
 app.use("/video",videoRoutes)
+app.use("/admin",admin)
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
