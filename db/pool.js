01b5dcdaa552820b31012becdf313b141ab2b003
sarvesh Kharoshe <sarveshkharoshe8@gmail.com>
2025-12-22 10:29:27 +0530

    adding students routes

---
+++ db/pool.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool=mysql.createPool({
+    host: 'localhost',
+    user:'root',
+    password:'Admin',
+    database:'mern_db'
+})
+
+module.exports= pool ;
+
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const courseRoutes=require('./routes/courseRoutes')
+const studrouter=require("./routes/student")
+const videoRoutes=require("./routes/videoRouters")
+
+const app=express();
+
+app.use(express.json());
+app.use('/courses',courseRoutes)
+app.use("/student",studrouter)
+app.use("/video",videoRoutes)
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
