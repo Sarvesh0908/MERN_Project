4828dd7fa778f66faa61d7940d8e1eb6cc8d32c5
Prathamesh vani <Prathameshvani20@gmail.com>
2025-12-30 23:29:45 +0530

    profile

--- Frontend/User/App1/src/App.jsx
+++ Frontend/User/App1/src/App.jsx
@@ -1,6 +1,9 @@
 import {Route,Routes,BrowserRouter} from "react-router"
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
+import profile from './Pages/Profile'
+
+
 
 
 
@@ -15,6 +18,7 @@ function App() {
         
         <Route path ="/Login" element={<Login/>} />
      <Route path ="/Signup"element={<Signup/>} />
+      <Route path ="/profile" element={<profile/>} />
 
       </Routes>
   </BrowserRouter>
