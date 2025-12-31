1657e4d95bdc7c9bfa7c56c85201d8f4131c0ab9
Aditya hogade <adityahogade2612@gmail.com>
2025-12-31 09:00:42 +0530

    course page

--- Frontend/Admin/app1/src/services/course.js
+++ Frontend/Admin/app1/src/services/course.js
@@ -18,3 +18,27 @@ export async function getActiveCourses(token){
     return result.data
 
 }
+
+export async function addCourses(token,data){
+    const url= config.BASE_URL+'/courses/add'
+    const body=data
+    const headers={token}
+
+    const response = await axios.post(url, body, { headers })
+    return response.data
+}
+
+export async function deleteCourse(token,id){
+    const url=config.BASE_URL+`/courses/delete/${id}`
+    const headers={token}
+    const result= await axios.delete(url,{headers})
+    return result.data
+}
+
+export async function updateCourseDetails(id,data,token){
+    const url=config.BASE_URL+`/courses/update/${id}`
+    const body=data
+    const headers={token}
+    const result= await axios.put(url,body,{headers})
+    return result.data
+}
