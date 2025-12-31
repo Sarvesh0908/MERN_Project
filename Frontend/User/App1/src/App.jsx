479004f4b518b23b97c0b7368416c56d278acf63
Jayraj Deshmukh <jayrajdeshmukh4199@gmail.com>
2025-12-31 17:31:47 +0530

    Routers added

--- Frontend/User/App1/src/App.jsx
+++ Frontend/User/App1/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route,Routes,BrowserRouter} from "react-router"
+import { Route, Routes, BrowserRouter } from "react-router"
 import Home from "./pages/home"
 import { Register } from "./pages/register_to_course"
 import Login from './Pages/Login'
@@ -7,22 +7,24 @@ import Signup from './Pages/Signup'
 
 
 function App() {
-  
+
 
   return (
     <>
-       
-    <BrowserRouter>
-  <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/register/:id" element={<Register />} />
-        
-        <Route path ="/Login" element={<Login/>} />
-     <Route path ="/Signup"element={<Signup/>} />
 
-      </Routes>
-  </BrowserRouter>
-  </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/register/:id" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/mycourses" element={<Mycourses />} />
+          <Route path="/mycourseswithvideo/:course_id" element={<VideosByCourse />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/changepass" element={<ChangePassword />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
